e9e4bde7e5a17ccda9012e59baf14b88949af71d
Gusseth <charleskplotado@gmail.com>
2021-02-10 16:22:20 -0800

    Modified to test new changed (ExpandTiler).

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ namespace fs = std::filesystem;
 using namespace TilerUtils;
 
 int main(int argc, char* argv[]) {
-	PNG input(fs::path("test/banner.png"));
+	PNG input(fs::path("test/shamei.png"));
 
 	Tiler tiler(16);
 	tiler.BuildTile(fs::path("test/lib/"), 16);
 
-	//PNG output = tiler.AverageTiler(input);
+	PNG output = tiler.ExpandTiler(input);
 
-	input.WriteFile(fs::path("test/test.png"));
+	output.WriteFile(fs::path("test/test1.png"));
 
 	return 0;
 }
